ae9b3bedfb743c5529be85a5b20e39560a15a6fe
Kairat Sultan <garri.sultonov@mail.ru>
2023-07-27 12:51:05 +0600

    add sentry django

--- api/core/settings.py
+++ api/core/settings.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import environ
@@ -225,4 +226,21 @@ if DEBUG is False:
             },
         },
     }
+    SENTRY_DSN = env("SENTRY_DSN")
+    SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
+
+    sentry_logging = LoggingIntegration(
+        level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
+        event_level=logging.ERROR,  # Send errors as events
+    )
+    integrations = [
+        sentry_logging,
+        DjangoIntegration(),
+    ]
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=integrations,
+        environment=env("SENTRY_ENVIRONMENT", default="production"),
+        traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=0.0),
+    )
 

1d83c01b1986c3f1d831ecbb18b5c9044e322ce7
Kairat Sultan <garri.sultonov@mail.ru>
2023-08-09 00:08:23 +0600

    fix

--- tgbot/keyboards/reply.py
+++ tgbot/keyboards/reply.py
@@ -7,5 +7,5 @@ _ = i18ns.gettext
 def main_menu_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(KeyboardButton(text=_("Менің жазылымым")))
-    keyboard.add(KeyboardButton(text=_("Менің өтінішім")))
+    keyboard.add(KeyboardButton(text=_("Менің өтініштерім")))
     return keyboard
